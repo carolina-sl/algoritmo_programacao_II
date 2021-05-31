b8d939036b894cd3020db4f98df206a0f166cfc0
Carolina Serafim Lauffer <carolina.lauffer@gmail.com>
2021-05-31 00:16:15 -0300

    lista duplamente encadeada
    Exercicio de lista duplamente encadeada.

--- exercicios/trabalho_lista_dupla_encadeada/No.py
+++ exercicios/trabalho_lista_dupla_encadeada/No.py
@@ -1,10 +1,3 @@
-'''
-
-Nó, funciona como um envelope;
-tem um espaço pra se colocar o dado
-e um espaço pra guardar a posição do nó seguinte com o proximo dado
-desse forma conseguimos ligar todos eles
-'''
 
 
 class No:
